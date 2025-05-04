c600221a8dc0c3af20901c871fa9a3249a30b9e8
KNereSouza <professional.neresk@gmail.com>
2025-05-03 22:29:56 -0300

    feat: set up database connection and server initialization with dotenv configuration

---
+++ src/config/database.js
@@ -0,0 +1,26 @@
+import { Sequelize } from "sequelize";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  dialect: "postgres",
+  host: dbHost,
+  logging: (...msg) => console.log(msg),
+});
+
+const authenticateToDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+export { authenticateToDatabase };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { authenticateToDatabase } from "./config/database.js";
 import { configDotenv } from "dotenv";
 
 import router from "./routes/routes.js";
@@ -9,8 +10,9 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
-
 app.use(router);
+
+authenticateToDatabase();
 
 app.listen(PORT, () => {
   console.log(`Server is running on: http://localhost:${PORT}`);
